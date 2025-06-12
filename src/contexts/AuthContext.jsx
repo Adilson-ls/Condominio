fc3105a7027b7d7157075806a5557635abf039d5
Adilson de Lima Santos <87918963+Adilson-ls@users.noreply.github.com>
2025-06-12 15:45:00 +0000

    up

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "../services/firebase/config";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
