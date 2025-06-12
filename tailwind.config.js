a24fb53aaa3acbff0c059b7f5626138e0882abb3
Adilson de Lima Santos <87918963+Adilson-ls@users.noreply.github.com>
2025-06-12 15:52:56 +0000

    instalando dependencias

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Ajuste conforme a estrutura do seu projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
